a3bc22b239cf4ad2186eee9e7d42a3d7b7445447
sitiqotijah <sitiqotijah01@gmail.com>
2020-04-16 11:03:38 +0700

    Updates

---
+++ precache-manifest.d9d0bfb0335afaa218d2f3595d66d8e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ee888a405f8e90d7d0fe4b6ae6734f9",
+    "url": "/UTS_Web-3/index.html"
+  },
+  {
+    "revision": "691a281dbd6fc562ded6",
+    "url": "/UTS_Web-3/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "167ff3db5ff83b348612",
+    "url": "/UTS_Web-3/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "691a281dbd6fc562ded6",
+    "url": "/UTS_Web-3/static/js/2.1e41281e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/UTS_Web-3/static/js/2.1e41281e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "167ff3db5ff83b348612",
+    "url": "/UTS_Web-3/static/js/main.e5e94785.chunk.js"
+  },
+  {
+    "revision": "e5788cc7e37ebe2f6ed5",
+    "url": "/UTS_Web-3/static/js/runtime-main.2fb038bc.js"
+  }
+]);
